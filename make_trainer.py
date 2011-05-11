049d7a427c9263aee321f8f2d8fe39c9cacd19f1
Michael Van Veen <michael@mvanveen.net>
2011-05-11 15:41:40 -0700

    updated for github

--- make_trainer.py
+++ make_trainer.py
@@ -2,11 +2,24 @@ import pickle
 import sys
 
 import milk
+import milk.supervised
+import milk.supervised.adaboost
+import milk.supervised.multi
 
 assert(sys.argv[1])
 features = pickle.load(open(sys.argv[1], 'r'))
-trainer  = milk.defaultlearner()
 
-model = trainer.train(*features)
+weak    = milk.supervised.tree.stump_learner()
+learner = milk.supervised.adaboost.boost_learner(weak)
+learner = milk.supervised.multi.one_against_one(learner)
+model = learner.train(*features)
 
 pickle.dump(model, open('trainer.pik', 'w'))
+
+cmat,names, preds = milk.nfoldcrossvalidation(*features,
+                                              classifier=learner,
+                                              return_predictions=1)
+
+print cmat
+print names
+print preds
